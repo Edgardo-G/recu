db0d7a72463dd881b1723db715edf630163ccfe1
edgardo-g <edgardogasp@hotmail.com>
2021-07-14 23:19:54 -0300

    arreglo el error y pas a sumar 4 + 4 en vez de 3 + 3

--- src/App.test.js
+++ src/App.test.js
@@ -3,7 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/Respuesta: 6/);
+  const linkElement = screen.getByText(/Respuesta: 16/);
   expect(linkElement).toBeInTheDocument();
 });
-// npm test
